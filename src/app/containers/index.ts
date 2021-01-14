d05b4fa1efb5ec4e989c8ce00893d5745a4249e6
sulha199 <sulha199@gmail.com>
2021-01-15 00:31:46 +0700

    update main page

--- src/app/containers/index.ts
+++ src/app/containers/index.ts
@@ -2,7 +2,7 @@ import { from } from 'rxjs';
 import { AppComponent} from './app/app.component';
 import { PerspectiveComponent } from './perspective/perspective.component';
 
-export {AppComponent, PerspectiveComponent}
+export {AppComponent, PerspectiveComponent};
 
 export const components = [
     AppComponent,
--- src/app/containers/perspective/perspective.component.ts
+++ src/app/containers/perspective/perspective.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { from, Observable } from 'rxjs';
+import { ApiPerspectiveService, PerspectiveService } from 'src/app/services';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-perspective',
@@ -6,10 +9,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./perspective.component.scss']
 })
 export class PerspectiveComponent implements OnInit {
+  readonly participant$ = this.perspectiveService.participant$;
+  readonly shouldShowPerspectiveResult$ = this.getShouldShowResult();
+  readonly questions$ = this.apiPerspective.getQuestions();
 
-  constructor() { }
+  constructor(
+    private perspectiveService: PerspectiveService,
+    private apiPerspective: ApiPerspectiveService
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  submit(): void {}
+
+  getShouldShowResult(): Observable<boolean> {
+    return this.participant$.pipe(
+      map(participant => !!participant
+        && !!participant.participantPerspective
+        && !!participant.participantPerspective.summary
+      )
+    );
   }
 
 }
