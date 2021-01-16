37e56eea9314d7dc1f6cb498d8af26ad8023dbe1
sulha199 <sulha199@gmail.com>
2021-01-16 15:44:38 +0700

    create participant form

---
+++ src/app/utils/perspetive.ts
@@ -0,0 +1,20 @@
+import { FormArray, FormControl, FormGroup, Validators } from "@angular/forms";
+import { Question } from "../models";
+
+export function createAnswerForm(): FormGroup {
+  return new FormGroup({
+    id: new FormControl(),
+    participant_id: new FormControl(),
+    question_id: new FormControl(null, Validators.required),
+    score: new FormControl(null, Validators.required)
+  });
+}
+
+export function createParticipantForm(questions: Question[]): FormGroup {
+  return  new FormGroup({
+    id: new FormControl(),
+    email: new FormControl(null, [Validators.required, Validators.email]),
+    timestamp: new FormControl(),
+    participantAnswers: new FormArray(questions.map(() => createAnswerForm()))
+  });
+}
