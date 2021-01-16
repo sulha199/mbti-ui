8615b5abbc83fb2b4a63cf6e1618744041c46b3e
sulha199 <sulha199@gmail.com>
2021-01-16 15:43:37 +0700

    add material and common module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
+import { MatButtonModule } from '@angular/material/button';
+import { MatRadioModule } from '@angular/material/radio';
 import * as containers from './containers';
 import * as components from './components';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [
@@ -13,7 +17,14 @@ import * as components from './components';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    CommonModule,
+
+    MatButtonModule,
+    MatRadioModule,
+
+    FormsModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [containers.AppComponent]
