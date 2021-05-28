348a7fcb0a930305f46459964485ba75a4ad1f3c
Shulha Yahya <sulha199@gmail.com>
2021-05-29 06:56:43 +0700

    create test on perspective service

--- src/app/services/api-perspective/api-perspective.service.spec.ts
+++ src/app/services/api-perspective/api-perspective.service.spec.ts
@@ -1,16 +1,42 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { ApiPerspectiveService } from './api-perspective.service';
+import { mockParticipant, mockParticipantWithPerspective, mockQuestions } from 'src/app/const';
+import { environment } from 'src/environments/environment';
 
-describe('ApiPerspectiveService', () => {
+fdescribe('ApiPerspectiveService', () => {
   let service: ApiPerspectiveService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule
+      ]
+    });
     service = TestBed.inject(ApiPerspectiveService);
+    httpMock = TestBed.inject(HttpTestingController);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  afterEach(() => {
+    httpMock.verify();
   });
+
+  it('should submit perspective result', (done) => {
+    service.submitPerspective(mockParticipant).subscribe(result => {
+      expect(result).toEqual(mockParticipantWithPerspective);
+      done();
+    });
+    httpMock.expectOne(`${environment.api}/${service.perspectivePath}/submit`).flush(mockParticipantWithPerspective);
+  });
+
+  it('should get question list', (done) => {
+    service.getQuestions().subscribe(result => {
+      expect(result).toEqual(mockQuestions);
+      done();
+    });
+    httpMock.expectOne(`${environment.api}/${service.questionPath}`).flush(mockQuestions);
+  });
+
+
 });
