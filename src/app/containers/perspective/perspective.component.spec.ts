b9f4f5862e44e21282e161c9f69712b5b7b533a5
Shulha Yahya <sulha199@gmail.com>
2021-05-29 12:39:15 +0700

    setup precommit as npm task

--- src/app/containers/perspective/perspective.component.spec.ts
+++ src/app/containers/perspective/perspective.component.spec.ts
@@ -1,5 +1,10 @@
 import { DebugElement } from '@angular/core';
-import { ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
+import {
+  ComponentFixture,
+  fakeAsync,
+  TestBed,
+  tick,
+} from '@angular/core/testing';
 import { MatButton } from '@angular/material/button';
 import { MatRadioButton, MatRadioGroup } from '@angular/material/radio';
 import { MatInput } from '@angular/material/input';
@@ -60,10 +65,12 @@ fdescribe('PerspectiveComponent', () => {
     fixture.detectChanges();
 
     // click submit button
-    spyOn(component, 'submit').and.callFake(formGroup => {
+    spyOn(component, 'submit').and.callFake((formGroup) => {
       const participantValue = formGroup.value as Participant;
       expect(formGroup.valid).toBe(true);
-      expect(participantValue.participantAnswers).toEqual(mockParticipant.participantAnswers);
+      expect(participantValue.participantAnswers).toEqual(
+        mockParticipant.participantAnswers
+      );
       expect(participantValue.email).toBe(mockParticipant.email);
     });
     expect(buttonInstance.disabled).toBeFalsy();
@@ -75,26 +82,36 @@ fdescribe('PerspectiveComponent', () => {
   it('submit() should send to API and navigate to result', fakeAsync(() => {
     const mockForm = createParticipantForm(mockQuestions);
     mockForm.patchValue(mockParticipant);
-    
+
     component.submit(mockForm);
     tick(100);
 
     expect(mockForm.valid).toBe(true);
-    expect(mockApiPerspectiveProvider.useValue.submitPerspective).toHaveBeenCalled();
-    expect(component.participant$.value).toEqual(mockParticipantWithPerspective);
-    expect(mockRouterProvider.useValue.navigate).toHaveBeenCalledWith([RouteList.result])
+    expect(
+      mockApiPerspectiveProvider.useValue.submitPerspective
+    ).toHaveBeenCalled();
+    expect(component.participant$.value).toEqual(
+      mockParticipantWithPerspective
+    );
+    expect(mockRouterProvider.useValue.navigate).toHaveBeenCalledWith([
+      RouteList.result,
+    ]);
   }));
 });
 
-function fillForm(radioGroupDebugElements: DebugElement[], emailInputElement: HTMLInputElement) {
+function fillForm(
+  radioGroupDebugElements: DebugElement[],
+  emailInputElement: HTMLInputElement
+) {
   radioGroupDebugElements.forEach((group, index) => {
     const radioDebugElements = group.queryAll(By.directive(MatRadioButton));
     const radioLabelElements: HTMLLabelElement[] = radioDebugElements.map(
       (debugEl) => debugEl.query(By.css('label')).nativeElement
     );
-    radioLabelElements[mockParticipant.participantAnswers[index].score - 1].click();
+    radioLabelElements[
+      mockParticipant.participantAnswers[index].score - 1
+    ].click();
   });
   emailInputElement.value = mockParticipant.email;
   emailInputElement.dispatchEvent(new Event('input'));
 }
-
