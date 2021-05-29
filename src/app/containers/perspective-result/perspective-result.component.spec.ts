1ab4cb8ea109a51dd58b54c13fdea0476e49ca93
Shulha Yahya <sulha199@gmail.com>
2021-05-29 11:15:35 +0700

    update unit test on PerspectiveResultComponent

--- src/app/containers/perspective-result/perspective-result.component.spec.ts
+++ src/app/containers/perspective-result/perspective-result.component.spec.ts
@@ -1,14 +1,23 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, fakeAsync, TestBed } from '@angular/core/testing';
+import { Router } from '@angular/router';
+import { mockParticipantWithPerspective } from 'src/app/const';
 
 import { PerspectiveResultComponent } from './perspective-result.component';
 
 describe('PerspectiveResultComponent', () => {
+  const spyNavigate = jasmine.createSpy('navigate');
   let component: PerspectiveResultComponent;
   let fixture: ComponentFixture<PerspectiveResultComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ PerspectiveResultComponent ]
+      declarations: [ PerspectiveResultComponent ],
+      providers: [{
+        provide: Router,
+        useValue: {
+          navigate: spyNavigate
+        }}
+      ]
     })
     .compileComponents();
   });
@@ -19,7 +28,20 @@ describe('PerspectiveResultComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should redirect if result empty', () => {
     expect(component).toBeTruthy();
+    component.ngOnInit();
+
+    expect(spyNavigate).toHaveBeenCalled();
   });
+
+  it('should show result', fakeAsync(() => {
+    spyNavigate.calls.reset();
+    component.participant$.next(mockParticipantWithPerspective);
+    component.ngOnInit();
+    fixture.detectChanges();
+
+    expect(spyNavigate).toHaveBeenCalledTimes(0)
+    expect(component).toBeTruthy();
+  }));
 });
